8ee5e1623c9d108d349fb4f4cd5c0fdd75527efb
btalakola <divya.cs237@gmail.com>
2022-04-02 16:10:15 +0530

    GatewaY api changed

---
+++ CLOUD-GATEWAY-1/src/main/java/com/ms/cloud/gateway1/CloudGateway1Application.java
@@ -0,0 +1,16 @@
+package com.ms.cloud.gateway1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class CloudGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGateway1Application.class, args);
+	}
+
+}
